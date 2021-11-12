8f736863ed8a944252e85e1a537ab26356ea72d2
Subroto Karmokar <subroto35-1734@diu.edu.bd>
2021-11-12 18:45:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,186 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.e3jxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('sk-watch');
+        const productsCollection = database.collection('products');
+        const ordersCollection = database.collection('orders');
+        const reviewsCollection = database.collection('reviews');
+        const usersCollection = database.collection('users');
+
+        /* -------product------ */ 
+        // GET API
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            let products = [];
+            const count = await cursor.count();
+            if (page) {
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+            res.send({
+                count,
+                products
+            });
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
+        // POST API
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.json(result);
+        })
+
+        // DELETE ORDER API
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        /* -------Order------ */
+        // POST ORDER API
+        app.post('/orders', async (req, res) => {
+            const newOrder = req.body;
+            const result = await ordersCollection.insertOne(newOrder);
+            res.json(result);
+        })
+
+        // GET ORDER API
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            let query = {};
+            if(email){
+                query = { email: email };
+            }
+            // console.log(query);
+            const cursor = ordersCollection.find(query);
+            orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+
+        // DELETE ORDER API
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // UPDATE ORDER API
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateOrder = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upset: true };
+            const updateDoc = {
+                $set: {
+                    status: updateOrder.status
+                },
+            }
+            const result = await ordersCollection.updateOne(filter, updateDoc, options);
+            // console.log(result);
+            res.json(result);
+        })
+
+        /* -------Review------ */
+        // Review get api
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            reviews = await cursor.toArray();
+            res.json(reviews);
+        })
+
+        // Review post api
+        app.post('/reviews', async (req, res) => {
+            const newReview = req.body;
+            const result = await reviewsCollection.insertOne(newReview);
+            res.json(result);
+        })
+
+        /* -------User------ */
+        // user get api
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+
+        // user Post Api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            // console.log(result);
+            res.json(result);
+        });
+
+        // user Put Api
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        // User Admin Put Api
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('This is home');
+});
+
+app.get('/test', (req, res) => {
+    res.send('This is test');
+});
+
+app.listen(port, () => {
+    console.log('server is up and running at', port);
+})
